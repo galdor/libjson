99847ce2aa9954317048a13b875e9773fa8fc426
Nicolas Martyanoff <khaelin@gmail.com>
2014-04-05 22:45:35 +0200

    fix json_decode_hex_digit()

--- src/parser.c
+++ src/parser.c
@@ -723,9 +723,9 @@ static int
 json_decode_hex_digit(unsigned char c) {
     if (c >= '0' && c <= '9') {
         return c - '0';
-    } else if (c >= 'a' || c <= 'z') {
+    } else if (c >= 'a' && c <= 'f') {
         return 10 + c - 'a';
-    } else if (c >= 'A' || c <= 'Z') {
+    } else if (c >= 'A' && c <= 'F') {
         return 10 + c - 'A';
     } else {
         return -1;
